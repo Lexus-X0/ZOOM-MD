064aafaaeb3ae699bc84a64733cf6d0d37f247b5
Lexux-XX <157197548+Lexus-X0@users.noreply.github.com>
2024-03-15 12:44:06 +0530

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -17,7 +17,7 @@ var res = await axios.get(`https://api-viper-x.koyeb.app/api/song?name=${match}`
 var song = res.data
 await message.client.sendMessage(message.jid, { text: `*_Downloading ${song.data.title}_*` },{ quoted: message})
 const aswinsparky = await (await fetch(`${song.data.downloadUrl}`)).buffer()
-await message.client.sendMessage(message.jid, { audio :aswinsparky,  mimetype:"audio/mpeg",
+await message.client.sendMessage(message.jid, { audio :aswinsparky,  mimetype:"audio/mpeg")
     }
     );
 
