d3251dab1feeab0c51cf6993ae1a03706e28dc79
Lexux-XX <157197548+Lexus-X0@users.noreply.github.com>
2024-03-16 00:26:47 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -17,14 +17,6 @@ var res = await axios.get(`https://api-viper-x.koyeb.app/api/song?name=${match}`
 var song = res.data
 await message.client.sendMessage(message.jid, { text: `*_Downloading ${song.data.title}_*` },{ quoted: message})
 const aswinsparky = await (await fetch(`${song.data.downloadUrl}`)).buffer()
-await message.client.sendMessage(message.jid, { audio :aswinsparky,  mimetype:"audio/mpeg", contextInfo: { externalAdReply: {
-title: "𝐄𝐙𝐑𝐀-𝐗𝐃",
-body: "𝙎𝙤𝙣𝙜 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙𝙚𝙙 𝙎𝙪𝙘𝙘𝙚𝙨𝙨𝙛𝙪𝙡𝙡𝙮",
-sourceUrl: "",
-mediaUrl: "",
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/UdydsAj.jpeg" }} }, {quoted: message })
+await message.client.sendMessage(message.jid, { audio :aswinsparky }, {quoted: message })
     }
     );
